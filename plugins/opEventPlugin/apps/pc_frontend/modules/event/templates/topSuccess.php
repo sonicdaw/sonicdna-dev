eec34d512da6122f9a3cff002078d619b1c28f80
sonicdna <mshd@da.mbn.or.jp>
2017-05-16 00:57:28 +0900

    Removed not used comments adn descriptions

--- plugins/opEventPlugin/apps/pc_frontend/modules/event/templates/topSuccess.php
+++ plugins/opEventPlugin/apps/pc_frontend/modules/event/templates/topSuccess.php
@@ -1,20 +1,3 @@
-<div class="partsHeading2"><h3>Create music events and connect with people</h3></div>
-<table bgcolor=white width=600><tr>
-<td width=20></td><td><b><font size=3>Be an event Organizer</font></b></td>
-<td>Create an event and connect with people</td>
-</tr><tr>
-<td></td><td><b><font size=3>Be players</font></b></td>
-<td>Create a band and share your music</td>
-</tr><tr>
-<td></td><td><b><font size=3>Be Audiences</font></b></td>
-<td>Find and join an event</td>
-</tr><tr>
-<td></td><td><b><font size=3>Be Staff</font></b></td>
-<td>Collaborate in an event</td>
-</tr></table>
-<br><br><br>
-
-
 <?php if ($pager->getNbResults()): ?>
 
 <?php
@@ -23,12 +6,12 @@ foreach ($pager->getResults() as $key => $event)
 {
   $list[$key] = array();
   $list[$key][__('%event% Name', array('%event%' => $op_term['event']->titleize()))] = $event->getName();
-  $list[$key][__('Count of Members')] = $event->countEventMembers();
+//  $list[$key][__('Count of Members')] = $event->countEventMembers();
   $list[$key][__('Description')] = $event->getConfig('description');
 }
 
 $options = array(
-  'title'          => __('Upcoming Events'),
+  'title'          => __('SONIC DNA'),
   'pager'          => $pager,
   'link_to_page'   => '@event_search?page=%d',
   'link_to_detail' => '@event_home?id=%d',
